a80cc40382696bc355f6cd8d0bebab6cf5a4f022
S Mohammed Rafi <smohammedrafi2003@gmail.com>
2024-05-25 00:41:05 +0530

    gitready

--- file1.js
+++ file1.js
@@ -1,2 +1,3 @@
-console.log("Version 3");
-console.log("Welcome you are version 3");
+console.log("Version 1");
+console.log("Welcome you are version 1");
+console.log("Now we are ready with GITHUB");
