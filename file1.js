fdd59a20e06531cca2d51f7d8be0e8ff877471a4
S Mohammed Rafi <smohammedrafi2003@gmail.com>
2024-05-25 01:01:04 +0530

    Version 15

--- file1.js
+++ file1.js
@@ -3,3 +3,5 @@ console.log("Welcome you are version 1");
 console.log("Now we are ready with GITHUB");
 console.log("Small change");
 console.log("now some more change");
+
+console.log("now im changing from the git clone ");
